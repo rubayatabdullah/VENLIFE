076c86f297f32d4041d5990bc916f685a27dbc8e
Rubayat Abdullah <114306964+rubayatabdullah@users.noreply.github.com>
2024-12-17 17:09:03 +0600

    added nested van detail

--- src/components/Host/HostVanPhotos.jsx
+++ src/components/Host/HostVanPhotos.jsx
@@ -2,5 +2,5 @@ import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPhotos() {
   const { currentVan } = useOutletContext();
-  return <h1>Host van photos</h1>;
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
 }
--- src/components/Host/HostVansPricing.jsx
+++ src/components/Host/HostVansPricing.jsx
@@ -2,5 +2,10 @@ import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPricing() {
   const { currentVan } = useOutletContext();
-  return <h1>Host van pricing</h1>;
+  return (
+    <h3 className="host-van-price">
+      ${currentVan.price}
+      <span>/day</span>
+    </h3>
+  );
 }
